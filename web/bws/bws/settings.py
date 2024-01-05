a755b4bf666bd22e3763e34569117bfafff0fb8c
Rogério <rogerio.dosantos@ifpr.edu.br>
2024-01-04 21:00:09 -0300

    Update settings.py

--- web/bws/bws/settings.py
+++ web/bws/bws/settings.py
@@ -49,7 +49,7 @@ SECRET_KEY = env.str('SECRET_KEY', default=get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=True)
 
-ALLOWED_HOSTS = env('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 PROD = env.bool('PROD', default=False)
 
 # Application definition
